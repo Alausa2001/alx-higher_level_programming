739020cd9d6614a526738f6ec1865aa1a9d4bbf4
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-06-29 01:17:00 +0100

    fixed the issue by reducing the no of print() and accounting for string format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for a in range(00, 99):
-    if a < 10:
-        print("0{}".format(a), end= ', ')
-    else:
-        print("{}".format(a), end= ', ')
-print(99)
+    print("{:02d}".format(a), end= ', ')
+print("{:02d}".format(a + 1))
