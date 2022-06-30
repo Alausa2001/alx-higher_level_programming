8e901f115e48e3ab0f36f7acbab4622a585e4e8a
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-06-30 01:59:33 +0100

    fizzbuzz task

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for n range(1, 101):
+    if number % 5 == 0 and number % 3 == 0:
+        print("FizzBuzz", end=' ')
+    elif number % 3 == 0:
+        print("Fizz", end=' ')
+    elif number % 5 == 0:
+        print("Buzz", end=' ')
+    else:
+        print(number, end=' ')
