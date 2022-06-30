fd9aa65ec84eca0718be47e12ceeac50fc5523a1
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-01 00:12:57 +0100

    task1 import and modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add as AD
+AD(1, 2)
+
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,3 @@
+def add(a, b):
+    return(print("{} + {} = {}".format(a, b, a + b)))
+#add(1, 2)
