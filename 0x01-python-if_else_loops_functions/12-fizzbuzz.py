9c7a0dfccdfb9b5614ed91ee55836fc985a84900
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-06-30 02:15:57 +0100

    added the prototype

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-for number in range(1, 101):
-    if number % 5 == 0 and number % 3 == 0:
-        print("FizzBuzz", end=' ')
-    elif number % 3 == 0:
-        print("Fizz", end=' ')
-    elif number % 5 == 0:
-        print("Buzz", end=' ')
-    else:
-        print(number, end=' ')
-print(end='$')
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 5 == 0 and number % 3 == 0:
+            print("FizzBuzz", end=' ')
+        elif number % 3 == 0:
+            print("Fizz", end=' ')
+        elif number % 5 == 0:
+            print("Buzz", end=' ')
+        else:
+            print(number, end=' ')
