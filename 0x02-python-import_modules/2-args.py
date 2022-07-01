2482b17018828983f1b03381d7bf06a0c99ce438
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-01 02:19:40 +0100

    made corrections to the indentations

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "__main__"
-lists = (sys.argv)
-b = 1
-if len(lists) > 2:
-    print(f"{len(lists) -  b} arguments")
-    for a in range(1, len(lists)):
-        print(f"{a}", end=': ')
-        print(lists[a])
-elif len(lists) == 2:
-    print(f"{len(lists) - b} argument")
-    for a in range(1, len(lists)):
-        print(f"{a}", end=': ')
-        print(lists[a])
-elif len(lists) == 1:
-    print(f"{len(lists) - b} arguments")
+if __name__ == "__main__":
+    lists = (sys.argv)
+    b = 1
+    if len(lists) > 2:
+        print("{} arguments".format(len(lists) - 1))
+        for a in range(1, len(lists)):
+            print(f"{a}", end=': ')
+            print(lists[a])
+    elif len(lists) == 2:
+        print(f"{len(lists) - b} argument")
+        for a in range(1, len(lists)):
+            print(f"{a}", end=': ')
+            print(lists[a])
+    elif len(lists) == 1:
+        print(f"{len(lists) - b} arguments")
