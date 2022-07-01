6b891b890300b62ec0c317bab21d1551ae27862a
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-01 02:27:54 +0100

    corrected the output text

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,14 @@ if __name__ == "__main__":
     lists = (sys.argv)
     b = 1
     if len(lists) > 2:
-        print("{} arguments".format(len(lists) - 1))
+        print("{} arguments:".format(len(lists) - 1))
         for a in range(1, len(lists)):
-            print(f"{a}", end=': ')
+            print("{}".format(a), end=': ')
             print(lists[a])
     elif len(lists) == 2:
-        print(f"{len(lists) - b} argument")
+        print("{} argument:".format(len(lists) - 1))
         for a in range(1, len(lists)):
-            print(f"{a}", end=': ')
+            print("{}".format(a), end=': ')
             print(lists[a])
     elif len(lists) == 1:
-        print(f"{len(lists) - b} arguments")
+        print("{} arguments.".format(len(lists) - 1))
