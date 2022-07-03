e7d18164b3b679433976cf4cf4a91f50692cab48
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-03 21:15:51 +0100

    added the path to the interpreter and deleted a print statement

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,3 +5,7 @@ def uppercase(str):
             a = ord(a) - 32
         print("{}".format(chr(a)), end='')
     print()
+no_of_daysuser = 365 * age_of_userconvert
+noofweeksuser = 52 * age_of_userconvert
+noofmonthuser = 12 * age_of_userconvert
+print(f("what {noofweeksuser}"))
