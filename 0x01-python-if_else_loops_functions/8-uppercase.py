ad329740c90efc8a5805c2fe259b0bad6f71afa4
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-03 21:15:17 +0100

    added the path to the interpreter and deleted a print statement

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,5 @@ def uppercase(str):
     for a in str:
         if ord(a) >= 97 and ord(a) <= 122:
             a = ord(a) - 32
-            print("{}".format(chr(a)), end='')
-        else:
-            print("{}".format(a), end='')
+        print("{}".format(chr(a)), end='')
+    print()
