b4ce8aae25a82ccd98cbdd4ce405508c96ccb7a2
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-03 21:24:18 +0100

    corrected the position chr() which does to conversion to alphabet

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for a in str:
         if ord(a) >= 97 and ord(a) <= 122:
-            a = ord(a) - 32
-        print("{}".format(chr(a)), end='')
+            a = chr(ord(a) - 32)
+        print("{}".format(a), end='')
     print()
