d56f9adc6fd9b196143f753d35af05b8abfe58cc
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-05 02:09:37 +0100

    made account for if idx >= 0

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for item in range(len(my_list)):
-        if idx == item and idx > 0:
+        if idx == item and idx >= 0:
             return(my_list[item])
         elif idx == item and idx < 0:
             return(None)
