e9320b34ccca1b10b296a0a13389513e688c8e2d
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-05 03:14:45 +0100

    accounted fr empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for item in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[item]))
+    if len(my_list) >= 1:
+        for item in range(len(my_list) - 1, -1, -1):
+            print("{:d}".format(my_list[item]))
