0d438df51cc104d9d35d7252fc8c04250a6921e4
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-05 04:02:58 +0100

    accounted for codestyle

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for a in range(len(my_string)):
-        if my_string[a]  != 'c':
-            if my_string[a]  != 'C':
+        if my_string[a] != 'c':
+            if my_string[a] != 'C':
                 print(my_string[a], end='')
     return('')
-
