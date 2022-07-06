f4ca6c3c9b129199685b56272dadbbc4d48dc17b
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-06 08:17:17 +0100

    corrected my code that it works for lists only and it will not give error for non-list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) >= 1:
-        for item in range(len(my_list) - 1, -1, -1):
-            print("{:d}".format(my_list[item]))
+    if type(my_list) is list:
+        if len(my_list) >= 1:
+            for item in range(len(my_list) - 1, -1, -1):
+                print("{:d}".format(my_list[item]))
