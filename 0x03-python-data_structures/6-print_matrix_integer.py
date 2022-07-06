14f27084736316e7c55b20a2f656f2e19b797b15
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-06 07:18:56 +0100

    corrected my code: so that it won't give a space after printing the last integer in a row

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for a in range(len(matrix)):
         for b in range(len(matrix[a])):
-            print("{:d}".format(matrix[a][b]), end=' ')
-        print()
+            if b < len(matrix[a]) - 1:
+                print("{:d}".format(matrix[a][b]), end=' ')
+            else:
+                print("{:d}".format(matrix[a][b]))
