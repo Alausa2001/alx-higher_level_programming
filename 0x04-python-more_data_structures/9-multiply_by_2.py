3598a3cc32d4cf25b656743b5779dfecdcaa80cf
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-07 03:23:04 +0100

    corrected

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     for a, b in a_dictionary.items():
-        if a in a_dictionary:
+        if a in a_dictionary and b in a_dictionary.values():
             a_dictionary[a] = b * 2
     return(a_dictionary)
