bbf8028fe6d3df9edac6b095a64306fb1d1bd8d1
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-07 03:28:06 +0100

    tried out dictionary comprehension

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for a, b in a_dictionary.items():
-        if a in a_dictionary and b in a_dictionary.values():
-            a_dictionary[a] = b * 2
-    return(a_dictionary)
+    return {i: j*2 for i, j in a_dictionary.items()}
