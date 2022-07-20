b6c3a5140f4d9d5916f99ed45d05d1607c95ec99
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-20 23:34:55 +0100

    a class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this module define a square with a private attribute (size)"""
+
+
+class Square:
+    """the class"""
+
+    def __init__(self, __size):
+        """this function Instantiate with size"""
+
+        self.__size = None
