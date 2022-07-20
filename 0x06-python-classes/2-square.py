105bcb50bf76dfd2ac0f6814dc977d96902dab50
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-21 00:21:47 +0100

    corrected the codestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,17 +2,17 @@
 """this module define a square with a private attribute (size)
 the size attribute can take only pisitve integer values"""
 
+
 class Square:
     """the class created, the type of size attribute is checked"""
 
     def __init__(self, __size=0):
-      
-        """this function Instantiate with size attribute with positive integers"""
-        
+
+        """this function Instantiate with size attribute with
+        positive integers"""
+
         if not isinstance(__size, int):
             raise TypeError("size must be an integer")
         elif __size < 0:
             raise ValueError("size must be >= 0")
-        
-
         self.__size = __size
