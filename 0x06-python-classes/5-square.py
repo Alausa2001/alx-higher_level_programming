9243daaf20a2df6f42bd28fa1a0cc92188bec959
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-21 02:40:17 +0100

    print a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -12,35 +12,36 @@ class Square:
         """this function Instantiate with size attribute with
         positive integers"""
         self.size = size
-        
+
     @property
     def size(self):
-        
+
         """ value getter"""
-        
+
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
-        
+
         """value setter"""
-        
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = value
+
     def area(self):
 
         """this method returns the area of the square"""
 
         return (self.__size ** 2)
-    
+
     def my_print(self):
-        
+
         """prints a square using #"""
-        
+
         if self.size == 0:
             print()
         else:
@@ -48,4 +49,3 @@ class Square:
                 for j in range(self.size):
                     print('#', end='')
                 print()
-            
