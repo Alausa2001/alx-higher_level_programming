8e31075b049a4e165cd1818474a820130033385f
Alausa Abdulqoyum Oluwaferanmi <101368817+Alausa2001@users.noreply.github.com>
2022-07-26 04:38:56 +0100

    Update 1-rectangle.py
    initialized as a public attribute

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """initialze the instances with the
             varible names"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
