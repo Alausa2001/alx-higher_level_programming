086d8f549898c4acc7b5bc2b13d831e1856b3e0f
Alausa Abdulqoyum Oluwaferanmi <101368817+Alausa2001@users.noreply.github.com>
2022-07-28 01:53:04 +0100

    Update 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """initialze the instances with the
             varible names"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __str__(self):
