f897d5b84ee2794b9f45406121ee7c3f990ca37b
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-31 15:27:30 +0100

    matrix miltplication using numpy model

---
+++ 0x07-python-test_driven_development/tests/101-lazy_matrix_mul.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""matrix multiplication using numpy"""
+
+import numpy as np
+def lazy_matrix_mul(m_a, m_b):
+    """matrix multiplication: two list are taken as arguments"""
+    return (np.matmul(m_a, m_b))
