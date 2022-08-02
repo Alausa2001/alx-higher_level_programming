bf6719c3a45e4d1ba47571d47d0b87f22caa55e9
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-08-02 05:01:22 +0100

    using super() to override

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -43,11 +43,4 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        """returns the area of the square"""
-        return (self.__size ** 2)
-
-    def __str__(self):
-        """print the result"""
-        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
+        super().__init__(self.__size, self.__size)
