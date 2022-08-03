ff2fe48eb3a7d1fddbf0e8a9db27f5c4d9724dee
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-08-03 10:32:01 +0100

    made Class Rectangle a subclass of BaseGeometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,20 +2,7 @@
 """contains a class that has method ``area``"""
 
 
-class BaseGeometry:
-    """has two methods (area() and integer_validator()"""
-
-    def area(self):
-        """raises an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """checks if the value of name is an integer
-        if True then check if the value is greater than 0"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
