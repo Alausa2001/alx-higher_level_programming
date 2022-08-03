4b4e3f217c3a8be6eed8aea03c274563d11415b4
Alausa Abdulqoyum Oluwaferanmi <101368817+Alausa2001@users.noreply.github.com>
2022-08-03 02:23:32 +0100

    function that reads a fileand prints it to stdout:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """READS A FILE TO STDOUT"""
-    with open('UTF8', 'r', encoding='utf-8') as f:
+    with open('filename', 'r', encoding='utf-8') as f:
         result = f.read()
         print(result)
