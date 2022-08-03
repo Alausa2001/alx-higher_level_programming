f17cafca595d2cbabd491e4c62dec9dab20cc549
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-08-03 10:46:30 +0100

    debugged

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -50,4 +50,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """prints the result"""
-        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
+        return ("[Square] {}/{}".format(self.__size, self.__size))
