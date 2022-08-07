adb16655294ce36f8477c9073235fd0b9150e858
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-08-07 19:42:38 +0100

    updated retangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,10 +24,14 @@ class Rectangle(Base):
         return (self.__width)
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """sets the width of the rectctangle to a
         private class instance ``__width``"""
-        self.__width = width
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -35,10 +39,14 @@ class Rectangle(Base):
         return (self.__height)
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """assigns the height of the rectangle to a private class
         instance ``__height"""
-        self.__height = height
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
@@ -46,9 +54,13 @@ class Rectangle(Base):
         return (self.__x)
 
     @x.setter
-    def x(self, x):
+    def x(self, value):
         """sets the value of x"""
-        self.__x = x
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
     @property
     def y(self):
@@ -56,6 +68,10 @@ class Rectangle(Base):
         return (self.__y)
 
     @y.setter
-    def y(self, y):
+    def y(self, value):
         """sets the value of y"""
-        self.__y = y
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
--- 0x0C-python-almost_a_circle/models/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" 1-main """
-from rectangle import Rectangle
-from base import Base
-
-if __name__ == "__main__":
-    print(issubclass(Rectangle, Base))
