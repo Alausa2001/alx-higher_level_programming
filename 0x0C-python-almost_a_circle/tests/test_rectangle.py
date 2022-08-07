6107c30bc006d43c65eaa294e9e4c351ab098a2f
Alausa Abdulqoyum Oluwaferanmi <101368817+Alausa2001@users.noreply.github.com>
2022-08-07 18:31:57 +0100

    Create test_rectangle.py

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,51 @@
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import pep8
+import json
+
+
+class TestRectangleClass(unittest.TestCase):
+
+    @classmethod
+    def setUp(cls):
+        Base._Base__nb_objects = 0
+        cls.rect1 = Rectangle(2, 4)
+        cls.rect2 = Rectangle(2, 4, 6)
+        cls.rect2 = Rectangle(2, 4, 6, 8)
+        cls.rect2 = Rectangle(2, 4, 6, 8, 10)
+
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+    def test_class(self):
+        '''
+        Rectangle is a class
+        '''
+        self.assertTrue(str(Rectangle), "<class 'models.rectangle.Rectangle'>")
+
+    def test_B_inheritance(self):
+        '''
+        Tests if Rectangle inherits Base.
+        '''
+        self.assertTrue(issubclass(Rectangle, Base))
+
+    def test_id_4(self):
+        b1 = Base(1)
+        b2 = Base(2)
+        b3 = Base(3)
+        b4 = Base(69)
+        b5 = Base(4)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 69)
+        self.assertEqual(b5.id, 4)
+
+    def test_id(self):
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 4)
+        r2 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r2.id, 12)
