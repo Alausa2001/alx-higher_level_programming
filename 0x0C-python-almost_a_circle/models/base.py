5ff4dfee968601b0b267c8849e8a4be79487bc0b
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-08-08 23:52:17 +0100

    added a method to class 'Base', this method returns the json representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns a json representation of list_dictionaries"""
-        if list_dictionaries is None or if not list_dictionaries:
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
