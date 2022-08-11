d2452cd39163aa04e93d290fb3809bec0d52945c
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-08-11 21:34:03 +0100

    added a test for Square instance creation and test for the values passed to the args

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -9,12 +9,18 @@ from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
+    """Test the functionality of the Square class"""
     
-
-    def test_Square_instance(self):
+    @classmethod
+    def test_class_setup(cls):
         """test square instance creation"""
+        Base._Base__nb_objects = 0
         sqr = Square(1, 2, 3, 4)
         sqr2 = Square(1, 2, 3)
+    def test_arg_values(self):
+        sqr = Square(1, 2, 3, 4)
+        sqr2 = Square(1, 2, 3)
+
         self.assertEqual(sqr.size, 1)
         self.assertEqual(sqr.x, 2)
         self.assertEqual(sqr.y, 3)
