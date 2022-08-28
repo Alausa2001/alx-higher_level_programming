60add786f2205832aa88fdfea2ed3ac576d8a48d
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-08-28 15:31:48 +0100

    handles exception and prints it to standard output

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-from sys import stderr
+import sys
 
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return(True)
-    except ValueError as error:
-        # stderr.write(error)
-        print("Exception: {}".format(error), file=stderr)
+    except (TypeError, ValueError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
