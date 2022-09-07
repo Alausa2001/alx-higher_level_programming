72210b7fffbd6cc2fedebbe4968ed2403003e990
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-08 00:09:38 +0100

    insert some values into a table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+
+-- New row:
+--   id = 89
+--   name = Best School
+INSERT INTO first_table VALUES (89, 'Best School');
