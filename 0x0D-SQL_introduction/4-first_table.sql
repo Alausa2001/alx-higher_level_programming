8bfacd56cff9668cb462b6f7e7cd0d35e5098b16
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-07 23:26:15 +0100

    creates a table called first_table in the current database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- script that creates a table called first_table
+-- first_table description:
+--               id INT
+--               name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (id int, name varchar(256));
