0a976454a473f690eeb697620dd8a7b0bf170208
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-08 17:04:15 +0100

    counts the number of individual with the same score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+--  script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
+
+-- The result should display:
+--         the score
+--         the number of records for this score with the label number
+-- The list should be sorted by the number of records (descending)
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
