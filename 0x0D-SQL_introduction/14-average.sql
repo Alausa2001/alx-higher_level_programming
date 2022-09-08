15e9982d5a53d512b2c25dd87a9b88dd0c0a6068
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-08 11:36:21 +0100

    gets the average of the 'score' column

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
--- script that updates the score of Bob to 10 in the table second_table.
+-- script that calculates the average score in the table second_table.
 
 SELECT AVG(score) AS average FROM second_table;
