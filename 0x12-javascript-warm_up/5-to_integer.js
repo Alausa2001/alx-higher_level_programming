2e919ae16be38c6b513cd50ce3fa79eb31389dc0
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-10-04 00:13:27 +0100

    type conversion

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const number = Number(process.argv[2]);
-if (!(isNaN(number)))
-	console.log(`My number: ${number}`);
-else
-	console.log('Not a number');
+if (!(isNaN(number))) {
+  console.log(`My number: ${number}`);
+} else {
+  console.log('Not a number');
+}
