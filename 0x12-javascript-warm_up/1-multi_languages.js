5a5ffa937cff090ed8af8b6b419cb4969af3cd78
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-10-03 16:54:27 +0100

    3 languagesclear :). Isn.t that awesome??

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const c = 'C is fun';
 const python = 'Python is cool';
-const Javascript = 'Javascript is amazing';
+const Javascript = 'JavaScript is amazing';
 console.log(c);
 console.log(python);
 console.log(Javascript);
