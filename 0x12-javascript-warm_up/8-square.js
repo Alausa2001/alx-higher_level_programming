afa90ed039deae68670fa200b88730245e61d5c8
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-10-04 01:22:28 +0100

    script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      process.stdout.write('X');
+    }
+    process.stdout.write('\n');
+  }
+}
