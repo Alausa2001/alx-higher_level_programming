4b88e3a2ecf423338924d0e73c8e5fbca2e8583a
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-10-08 00:57:17 +0100

    added condition for if the state being is not added as an argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -23,10 +23,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}'  ORDER BY id ASC"
-                .format(argv[4]))
-    rows = cur.fetchall()
-    cur.close()
-    db.close()
-    for row in rows:
-        print(row)
+    if len(argv) == 5:
+        cur.execute("SELECT * FROM states WHERE name='{}'  ORDER BY id ASC"
+                    .format(argv[4]))
+        rows = cur.fetchall()
+        cur.close()
+        db.close()
+        for row in rows:
+            print(row)
