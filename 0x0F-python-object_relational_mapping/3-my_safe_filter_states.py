f03c206870dce9d41aae72b9d0a70202d36b80b2
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-10-08 02:31:09 +0100

    prevented sql injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values i
+n the states table of hbtn_0e_0_usa where name matches the argument.
+
+Your script should take 4 arguments: mysql username, mysql password,
+database name and state name searched (no argument validation needed)
+
+You must use the module MySQLdb (import MySQLdb)
+
+Your script should connect to a MySQL server running on localhost at port 3306
+
+You must use format to create the SQL query with the user input
+
+Results must be sorted in ascending order by states.id
+
+Results must be displayed as they are in the example below
+
+Your code should not be executed when imported"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    if len(argv) == 5:
+        cur.execute("SELECT * FROM states WHERE name= %s  ORDER BY id ASC",
+                    (argv[4], ))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    cur.close()
+    db.close()
