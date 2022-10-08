09ab5a40b59478b0d80eafbc75caeb6870a3daf6
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-10-08 15:40:34 +0100

    corrected the codestyle

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
     if len(argv) == 5:
-        cur.execute('SELECT * FROM states WHERE name= \'{}\'
-                    ORDER BY id ASC'.format(argv[4]))
+        cur.execute('SELECT * FROM states WHERE name= \'{}\' ORDER BY id \
+                    ASC'.format(argv[4]))
         rows = cur.fetchall()
         for row in rows:
             print(row)
