cda69a7d7a7aff2182e6b04560fe2217a5206f16
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-10-09 12:21:11 +0100

    tried preventing SQL injections

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,13 +13,13 @@
  Your code should not be executed when imported
  """
 
-import MySQLdb as mysql
+import MySQLdb
 from sys import argv, stdout
 
 if __name__ == "__main__":
 
-    db = mysql.connect(host='localhost', port=3306, user=argv[1],
-                       passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
     av = argv[4]
     if (av.__contains__('TRUNCATE' or 'FROM' or 'SELECT')):
