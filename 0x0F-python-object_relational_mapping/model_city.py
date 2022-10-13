e6847e54b1cb5470d3e3df830792c7eb2efbceb6
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-10-13 23:24:23 +0100

    contains the class definition of a City.

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""contains the class definition of a City"""
+
+from model_state import State, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+
+class City(Base):
+    """attributes
+    table_name: cities
+    id: auto-generated, unique integer, can’t be null and is a primary key
+    name:  column of a string of 128 characters and can’t be null
+    state_id: state_id that represents a column of an integer,
+                can’t be null and is a foreign key to states.id
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True,
+                unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = (Integer, ForeignKey('states.id'))
