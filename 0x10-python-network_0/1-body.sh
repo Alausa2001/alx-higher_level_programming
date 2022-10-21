ddb529ea6f3c984532cc3744919929e8767eb1c2
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-10-21 22:03:21 +0100

    script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl --get -sL "$1" 
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the responsE
+curl -X GET -sL "$1" 
