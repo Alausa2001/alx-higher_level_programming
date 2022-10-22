1ed017f0eb97f63a4373054d0f9ad3b704765f64
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-10-22 22:50:00 +0100

    script that fetches https://alx-intranet.hbtn.io/status using requests package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ import requests
 if __name__ == "__main__":
     response = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print('\t- {}'.format(type(response.text)))
-    print('\t- {}'.format(response.text))
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
