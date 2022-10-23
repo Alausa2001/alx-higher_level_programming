fac889ed2ddb554aa97717c726b6000251d4971b
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-10-23 16:33:43 +0100

    Please list 10 commits (from the most recent to oldest) of the repository 'rails' by the user 'rails' You must use the GitHub API, here is the documentation 'https://developer.github.com/v3/repos/commits/' Print all commits by: <sha>: <author name> (one by line)

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     response = requests.get(que_str)
     commit_list = response.json()
     for commit in commit_list[:10]:
-        sha = commit.get('sha')
-        author = commit.get('commit').get('author').get('name')
+        sha = commit['sha']
+        author = commit['commit']['author']['name']
         print('{}: {}'.format(sha, author))
