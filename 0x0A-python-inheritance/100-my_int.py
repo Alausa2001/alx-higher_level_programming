413c754b8a5826485b44b2ce09815559acd379d5
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-01-15 08:07:33 +0100

    override the '==' and '!=' operators of python's class int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class that overides python's builtin class int"""
+
+
+class myInt(int):
+    """inherits from int"""
+
+    def __eq__(self, value):
+        """overrides the '==' operator"""
+        return self.real != value
+
+    def __ne__(self, value):
+        """overrides the '!=' operator"""
+        return self.real == value
